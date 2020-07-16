ad0bd9560b22662f4b901a1ebc31b70ef122fef2
juvenciolu <juvenciolugo@hotmail.com>
2020-07-16 18:59:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mgjaot
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mgjaot)
---
+++ index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Juvencio'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
