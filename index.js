7efc6b34df78ffd614ccf5873c6c1287daaaa858
juvenciolu <juvenciolugo@hotmail.com>
2020-07-16 20:24:19 -0500

    2 files modified

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ class Blog extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      articles:[
-        'Mi primer componente en React',
-        'Intro to react',
-        'Que es React?'
-      ]
-    }
+      articles:[]
+    };
   }
   componentDidMount(){
   //console.log("Estoy aqui");
   let promesa=fetch('https://jsonplaceholder.typicode.com/posts');
   promesa.then(res=>res.json()).then(data=>{
-    console.log(data);
+    //console.log(data);
+    this.setState({
+      articles:data
+    })
   })
 
 /*  o podria ser asi..
@@ -34,8 +33,8 @@ class Blog extends Component{
       <div>
       <h1>Mis Articulos</h1>
       {
-        this.state.articles.map((title)=>{
-          return <p>{title}</p>
+        this.state.articles.map((article)=>{
+          return <div className="card" style={{ backgroundColor:'red',color:'yellow' }}><p>{article.title}</p></div>
         })
       }
       </div>
