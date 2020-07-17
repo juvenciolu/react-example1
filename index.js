053847d20f3de626f4af653ddb8b78fd56b29969
juvenciolu <juvenciolugo@hotmail.com>
2020-07-16 19:21:57 -0500

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,46 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
+class Blog extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles:[
+        'Mi primer componente en React',
+        'Intro to react',
+        'Que es React?'
+      ]
+    }
+  }
+  componentDidMount(){
+  //console.log("Estoy aqui");
+  let promesa=fetch('https://jsonplaceholder.typicode.com/posts');
+  promesa.then(res=>res.json()).then(data=>{
+    console.log(data);
+  })
+
+/*  o podria ser asi..
+  promesa.then((res)=>{
+    res.json().then((data)=>{
+      console.log(data);
+    })
+  })*/
+
+}
+  render(){
+    return (
+      <div>
+      <h1>Mis Articulos</h1>
+      {
+        this.state.articles.map((title)=>{
+          return <p>{title}</p>
+        })
+      }
+      </div>
+    )
+  }
+}
+
 
 
 
@@ -18,6 +58,7 @@ class App extends Component {
     return (
       <div>
         <Hello name={this.state.name} />
+        <Blog />
 
       </div>
     );
